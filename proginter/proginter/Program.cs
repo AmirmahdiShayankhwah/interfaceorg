92a156d1404fb5c1723120850473cb7e57fc0077
AmirmahdiShayankhwah <amyrmhdyshaya7@gmail.com>
2023-07-17 22:56:52 +0430

    first inter

---
+++ proginter/proginter/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace proginter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            studentform stu = new studentform();
+
+            stu.avg();
+
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ proginter/proginter/studentform.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace proginter
+{
+    class studentform : Iprog
+    {
+        public void avg()
+        {
+            Console.WriteLine("my avg");
+        }
+
+        public void sum()
+        {
+            
+        }
+    }
+}
